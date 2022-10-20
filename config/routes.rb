8450ce63889ced92596781c4f683a23bf5e22747
Elson Otake <elsonotake@yahoo.com.br>
2022-10-20 20:57:48 -0300

    Create new route and create user action

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/auth/login', to: 'authentication#login'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[index show] do
+      resources :users, only: %i[index show create] do
         resources :bookings, only: %i[index show create]
       end
       resources :vehicles, only: %i[index show create] do
