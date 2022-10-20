9fccb2944f1ea218dbb7431e59f37453ebb4dca3
Elson Otake <elsonotake@yahoo.com.br>
2022-10-20 16:32:02 -0300

    Add create method

--- app/controllers/api/v1/galeries_controller.rb
+++ app/controllers/api/v1/galeries_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GaleriesController < ApplicationController
+  ALLOWED_DATA = %(photo).freeze
+
   def index
     vehicle = Vehicle.find_by_id!(params[:vehicle_id])
     galeries = vehicle.galeries
@@ -7,5 +9,18 @@ class Api::V1::GaleriesController < ApplicationController
     render json: { errors: 'Vehicle not found' }, status: :not_found
   end
 
-  def create; end
+  def create
+    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+    return render json: { error: 'Empty body. Could not create.' } if data.empty?
+
+    vehicle = Vehicle.find(params[:vehicle_id])
+    galery = vehicle.galeries.new(data)
+    if galery.save
+      render json: galery
+    else
+      render json: { error: 'could not create.' }
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Vehicle not found' }, status: :not_found
+  end
 end
