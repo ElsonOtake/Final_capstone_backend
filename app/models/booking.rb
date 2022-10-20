66c5f2f103da9f76d0f5210b0abac258ba9e6f17
Elson Otake <elsonotake@yahoo.com.br>
2022-10-19 22:40:23 -0300

    Add validation to booking, user, and vehicle models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vehicle
+  validates :duration, presence: true
+  validates :city, presence: true
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,8 @@
 class Vehicle < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  has_many :galeries, dependent: :destroy
+  validates :model, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1900 }
+  validates :acceleration, numericality: { only_integer: false, greater_than_or_equal_to: 0 }
 end
