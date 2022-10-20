ddc889bacb102a59d1eeb2ae32b1c03884402f56
Elson Otake <elsonotake@yahoo.com.br>
2022-10-20 17:31:25 -0300

    Add create action

--- app/controllers/api/v1/vehicles_controller.rb
+++ app/controllers/api/v1/vehicles_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::VehiclesController < ApplicationController
+  ALLOWED_DATA = %(model year brand color country power max_speed acceleration info_interior info_exterior price).freeze
+
   def index
     vehicles = Vehicle.all
     render json: vehicles
@@ -11,5 +13,17 @@ class Api::V1::VehiclesController < ApplicationController
     render json: vehicle
   rescue ActiveRecord::RecordNotFound
     render json: { errors: 'Vehicle not found' }, status: :not_found
+  end
+
+  def create
+    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+    return render json: { error: 'Empty body. Could not create vehicle.' } if data.empty?
+
+    vehicle = Vehicle.new(data)
+    if vehicle.save
+      render json: vehicle
+    else
+      render json: { error: 'Could not create vehicle.' }
+    end
   end
 end
