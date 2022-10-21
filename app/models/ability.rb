cda00305a7e531d0efe6f6cad9062a9f9e0b815e
Elson Otake <elsonotake@yahoo.com.br>
2022-10-20 22:48:58 -0300

    Fix linter error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     return unless user
 
-    can :manage, Booking, user: user
+    can :manage, Booking, user:
   end
 end
