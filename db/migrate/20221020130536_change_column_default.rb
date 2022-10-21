a1912094c6e2fe3f28e42887133ca9ac73a53f61
Elson Otake <elsonotake@yahoo.com.br>
2022-10-21 16:03:45 -0300

    Update columns booking table

--- db/migrate/20221020130536_change_column_default.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnDefault < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default(:vehicles, :year, 1900)
-    change_column_default(:vehicles, :acceleration, 0)
-  end
-end
