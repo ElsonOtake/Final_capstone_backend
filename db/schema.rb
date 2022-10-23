e73bee99aed624fd666c81444216d96ac0e4e274
Elson Otake <elsonotake@yahoo.com.br>
2022-10-22 22:45:07 -0300

    Update users column name to unique index

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_213820) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
