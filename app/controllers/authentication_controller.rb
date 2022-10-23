407769b2de9bc85b69fe0f51d5fa8b73b0a5fa59
Elson Otake <elsonotake@yahoo.com.br>
2022-10-22 22:06:58 -0300

    Update login by username instead of email

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,21 +1,21 @@
 class AuthenticationController < ApplicationController
   # protect_from_forgery unless: -> { request.format.json? }
 
-  ALLOWED_DATA = %(email password).freeze
+  ALLOWED_DATA = %(name password).freeze
 
   # POST /auth/login
   def login
     data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    @user = User.find_by_email!(data[:email])
+    @user = User.find_by_name!(data[:name])
     if @user.valid_password?(data[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
       render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     email: @user.email }, status: :ok
+                     name: @user.name, role: @user.role }, status: :ok
     else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+      render json: { error: 'Unauthorized' }, status: :unauthorized
     end
   rescue ActiveRecord::RecordNotFound
-    render json: { errors: 'User and/or email not found' }, status: :not_found
+    render json: { errors: 'Username not found' }, status: :not_found
   end
 end
