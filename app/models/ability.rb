79093a9c6450a11967272174512cdbbd22deb624
Elson Otake <elsonotake@yahoo.com.br>
2022-10-22 23:44:20 -0300

    Add abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,9 +1,15 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    return unless user
+  def initialize(current_user)
+    return unless current_user
 
-    can :manage, Booking, user:
+    if current_user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can %i[create], User
+      can %i[create], Booking, { user_id: current_user.id }
+    end
   end
 end
