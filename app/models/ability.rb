002490d2b2ebbf460f4866dc656cdb417c2f921d
Elson Otake <elsonotake@yahoo.com.br>
2022-10-24 16:47:59 -0300

    Update test order

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all
+    can :create, User
     return unless user
 
     if user.role == 'admin'
       can :manage, :all
     else
-      can :read, :all
-      can :create, User
       can :create, Booking, user_id: user.id
     end
   end
