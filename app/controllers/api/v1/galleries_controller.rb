185fb3b6b131b994ac1a327c6aa77c5c8108bbab
Elson Otake <elsonotake@yahoo.com.br>
2022-10-25 19:37:38 -0300

    Update return status

--- app/controllers/api/v1/galleries_controller.rb
+++ app/controllers/api/v1/galleries_controller.rb
@@ -7,21 +7,21 @@ class Api::V1::GalleriesController < ApplicationController
   def index
     vehicle = Vehicle.find_by_id!(params[:vehicle_id])
     galleries = vehicle.galleries
-    render json: galleries
+    render json: galleries, status: :ok
   rescue ActiveRecord::RecordNotFound
     render json: { errors: 'Vehicle not found' }, status: :not_found
   end
 
   def create
     data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    return render json: { error: 'Empty body. Could not create gallery.' } if data.empty?
+    return render json: { error: 'Empty body. Could not create gallery.' }, status: :unprocessable_entity if data.empty?
 
     vehicle = Vehicle.find(params[:vehicle_id])
     gallery = vehicle.galleries.new(data)
     if gallery.save
-      render json: gallery
+      render json: gallery, status: :ok
     else
-      render json: { error: 'Could not create gallery.' }
+      render json: { error: 'Could not create gallery.' }, status: :unprocessable_entity
     end
   rescue ActiveRecord::RecordNotFound
     render json: { errors: 'Vehicle not found' }, status: :not_found
