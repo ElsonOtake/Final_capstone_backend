efeb806057ddd0eb358f9583392d2f14a6d6207d
Elson Otake <elsonotake@yahoo.com.br>
2022-10-27 19:31:28 -0300

    Update admin permission

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::UsersController < ApplicationController
-  # load_and_authorize_resource
   before_action :authorize_request, except: %i[index create]
   before_action :find_user, except: %i[create index]
 
@@ -18,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /users
   def create
-    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) && allow != 'role' }
     return render json: { error: 'Empty body. Could not create user.' }, status: :unprocessable_entity if data.empty?
 
     user = User.new(data)
@@ -31,20 +30,28 @@ class Api::V1::UsersController < ApplicationController
 
   # PUT /users/:id
   def update
-    data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
-    return render json: { error: 'Empty body. Could not update user.' }, status: :unprocessable_entity if data.empty?
+    if current_user.is? :admin
+      data = json_payload.select { |allow| ALLOWED_DATA.include?(allow) }
+      return render json: { error: 'Empty body. Could not update user.' }, status: :unprocessable_entity if data.empty?
 
-    if @user.update(data)
-      render json: @user, status: :ok
+      if @user.update(data)
+        render json: @user, status: :ok
+      else
+        render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render json: { error: 'Unauthorized.' }, status: :unauthorized
     end
   end
 
   # DELETE /users/:id
   def destroy
-    @user.destroy
-    render json: @user, status: :ok
+    if current_user.is? :admin
+      @user.destroy
+      render json: @user, status: :ok
+    else
+      render json: { error: 'Unauthorized.' }, status: :unauthorized
+    end
   end
 
   private
