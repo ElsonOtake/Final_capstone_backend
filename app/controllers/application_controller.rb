29874296239a400e3232f68f93429e5af681c8ad
Elson Otake <elsonotake@yahoo.com.br>
2022-10-27 19:30:29 -0300

    Remove current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,27 +5,6 @@ class ApplicationController < ActionController::API
     HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
   end
 
-  # def current_user
-  #   puts "***** current_user #{current_user}"
-  #   return unless @current_user.nil?
-
-  #   header = request.headers['Authorization']
-  #   puts "***** header #{header}"
-  #   if header.nil?
-  #     @current_user = User.new
-  #   else
-  #     header = header.split.last
-  #     begin
-  #       @decoded = JsonWebToken.decode(header)
-  #       puts "***** decoded #{@decoded}"
-  #       @current_user = User.find_by_id!(@decoded[:user_id])
-  #       puts "***** current_user #{@current_user}"
-  #     rescue ActiveRecord::RecordNotFound || JWT::DecodeError
-  #       @current_user = User.new
-  #     end
-  #   end
-  # end
-
   def not_found
     render json: { error: 'not_found' }
   end
