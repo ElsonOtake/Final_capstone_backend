27a98c3cebe27e25366809661b7b9637e5b20275
Elson Otake <elsonotake@yahoo.com.br>
2022-10-27 16:44:04 -0300

    Add vehicles documentation

--- spec/integration/vehicles_spec.rb
+++ spec/integration/vehicles_spec.rb
@@ -1,14 +1,87 @@
 require 'swagger_helper'
 
-describe 'Users' do
-  path '/api/v1/users' do
-    get 'Retrieves users' do
-      tags 'Users'
-      consumes 'application/json'
+describe 'Vehicles' do
+  path '/api/v1/vehicles' do
+    get 'List vehicles' do
+      security [{ ApiKeyAuth: [] }]
+      tags 'Vehicles'
+      description 'Retrieves all vehicles'
+      produces 'application/json'
 
       response '200', 'OK' do
         schema type: :object, properties: {
-          id: { type: :integer },
+                                id: { type: :integer },
+                                model: { type: :string },
+                                description: { type: :string },
+                                year: { type: :string },
+                                brand: { type: :string },
+                                color: { type: :string },
+                                country: { type: :string },
+                                power: { type: :string },
+                                max_speed: { type: :string },
+                                acceleration: { type: :string },
+                                price: { type: :integer },
+                                created_at: { type: :string },
+                                updated_at: { type: :string }
+                              },
+               required: %w[model price]
+
+        let(:id) { Vehicle.create(model: 'foo', price: 100).id }
+        run_test!
+      end
+
+      response '401', 'Unauthorized' do
+        let(:id) { 'Unauthorized' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/vehicles/{id}' do
+    get 'Retrieve a vehicle' do
+      security [{ ApiKeyAuth: [] }]
+      tags 'Vehicles'
+      description 'Retrieves a vehicle'
+      produces 'application/json'
+
+      parameter name: :id, in: :path, type: :integer, required: true, description: 'Vehicle identification'
+
+      response '200', 'OK' do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer }, model: { type: :string }, description: { type: :string },
+                 year: { type: :string }, brand: { type: :string }, color: { type: :string },
+                 country: { type: :string }, power: { type: :string }, max_speed: { type: :string },
+                 acceleration: { type: :string }, price: { type: :integer },
+                 created_at: { type: :string }, updated_at: { type: :string }
+               },
+               required: %w[model price]
+
+        let(:id) { Vehicle.create(model: 'foo', price: 100).id }
+        run_test!
+      end
+
+      response '401', 'Unauthorized' do
+        let(:id) { 'Unauthorized' }
+        run_test!
+      end
+
+      response '404', 'Not found' do
+        let(:id) { 'Vehicle not found' }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/vehicles' do
+    post 'Create a vehicle' do
+      security [{ ApiKeyAuth: [] }]
+      tags 'Vehicles'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :vehicle, in: :body, description: 'Create a vehicle', schema: {
+        type: :object,
+        properties: {
           model: { type: :string },
           description: { type: :string },
           year: { type: :string },
@@ -18,46 +91,50 @@ describe 'Users' do
           power: { type: :string },
           max_speed: { type: :string },
           acceleration: { type: :string },
-          price: { type: :integer },
-          created_at: { type: :string },
-          updated_at: { type: :string }
-        }
-        let(:id) { 'valid' }
+          price: { type: :integer }
+        },
+        required: %w[model price]
+      }
+
+      response '200', 'OK' do
+        let(:vehicle) { { model: 'Fusca', price: 15 } }
+        run_test!
+      end
+
+      response '422', 'Unprocessable entity' do
+        let(:vehicle) { { model: 'Fusca' } }
         run_test!
       end
     end
   end
 
-  path '/api/v1/users/{id}' do
-    get 'Retrieve a user' do
+  path '/api/v1/vehicles/{id}' do
+    delete 'Delete a vehicle' do
       security [{ ApiKeyAuth: [] }]
-      tags 'Users'
+      tags 'Vehicles'
+      description 'Delete a vehicle'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :id, in: :path, type: :integer, required: true, description: 'Vehicle identification'
 
       response '200', 'OK' do
-        schema type: :object,
-               properties: {
-                 id: { type: :integer },
-                 model: { type: :string },
-                 description: { type: :string },
-                 year: { type: :string },
-                 brand: { type: :string },
-                 color: { type: :string },
-                 country: { type: :string },
-                 power: { type: :string },
-                 max_speed: { type: :string },
-                 acceleration: { type: :string },
-                 price: { type: :integer },
-                 created_at: { type: :string },
-                 updated_at: { type: :string }
-               }
-        let(:id) { :id }
-        run_test!
-      end
+        schema type: :object, properties: {
+                                id: { type: :integer },
+                                model: { type: :string },
+                                description: { type: :string },
+                                year: { type: :string },
+                                brand: { type: :string },
+                                color: { type: :string },
+                                country: { type: :string },
+                                power: { type: :string },
+                                max_speed: { type: :string },
+                                acceleration: { type: :string },
+                                price: { type: :integer },
+                                created_at: { type: :string },
+                                updated_at: { type: :string }
+                              },
+               required: %w[model price]
 
-      response '404', 'Not found' do
-        let(:error) { 'User not found' }
+        let(:id) { create(:vehicle).id }
         run_test!
       end
     end
