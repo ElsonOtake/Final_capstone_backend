2907e16bc4cddde2c5ec94aefa39e9c9f9b0ebcb
Elson Otake <elsonotake@yahoo.com.br>
2022-10-27 16:44:50 -0300

    Add galleries documentation

--- spec/integration/galleries_spec.rb
+++ spec/integration/galleries_spec.rb
@@ -1,60 +1,61 @@
 require 'swagger_helper'
 
-describe 'Posts' do
-  path '/api/v1/users/{id}/posts' do
-    get 'Retrieves posts' do
+describe 'Galleries' do
+  path '/api/v1/vehicles/{vehicle_id}/galleries' do
+    get 'Retrieves galleries' do
       security [{ ApiKeyAuth: [] }]
-      tags 'Posts'
+      tags 'Galleries'
+      description 'Retrieves all galleries'
       produces 'application/json'
-      parameter name: :id, in: :path, type: :string, required: true, description: 'user id'
+      parameter name: :vehicle_id, in: :path, type: :integer, required: true, description: 'Vehicle identification'
 
       response '200', 'OK' do
         schema type: :object, properties: {
-          id: { type: :integer },
-          start_date: { type: :string },
-          end_date: { type: :string },
-          city: { type: :string },
-          vehicle_id: { type: :integer },
-          user_id: { type: :integer },
-          created_at: { type: :string },
-          updated_at: { type: :string }
-        }
-        let(:comments_counter) { 0 }
+                                id: { type: :integer },
+                                photo: { type: :string },
+                                vehicle_id: { type: :integer },
+                                created_at: { type: :string },
+                                updated_at: { type: :string }
+                              },
+               required: %w[photo vehicle_id]
+
+        let(:vehicle_id) { Vehicle.create(model: 'foo', price: 100).id }
+        run_test!
+      end
+
+      response '401', 'Unauthorized' do
+        let(:vehicle_id) { 'Unauthorized' }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:error) { 'User/Post not found' }
+        let(:vehicle_id) { 'Gallery not found' }
         run_test!
       end
     end
   end
 
-  path '/api/v1/users/{user_id}/posts/{id}' do
-    get 'Retrieve a post' do
+  path '/api/v1/vehicles/{vehicle_id}/galleries' do
+    post 'Creates a gallery' do
       security [{ ApiKeyAuth: [] }]
-      tags 'Posts'
+      tags 'Galleries'
+      consumes 'application/json'
       produces 'application/json'
-      parameter name: :user_id, in: :path, type: :string, required: true, description: 'user id'
-      parameter name: :id, in: :path, type: :string, required: true, description: 'post id'
+      parameter name: :vehicle_id, in: :path, type: :integer, required: true, description: 'Vehicle identification'
+      parameter name: :id, in: :body, required: true, description: 'Gallery data', schema: {
+        type: :object, properties: {
+                         photo: { type: :string }
+                       },
+        required: %w[photo]
+      }
 
       response '200', 'OK' do
-        schema type: :object, properties: {
-        id: { type: :integer },
-        start_date: { type: :string },
-        end_date: { type: :string },
-        city: { type: :string },
-        vehicle_id: { type: :integer },
-        user_id: { type: :integer },
-        created_at: { type: :string },
-        updated_at: { type: :string }
-        }
-        let(:comments_counter) { 0 }
+        let(:id) { { photo: 'foo.png', vehicle_id: 1 } }
         run_test!
       end
 
-      response '404', 'Not found' do
-        let(:error) { 'User/Post not found' }
+      response '422', 'Unprocessable entity' do
+        let(:id) { { vehicle_id: 1 } }
         run_test!
       end
     end
