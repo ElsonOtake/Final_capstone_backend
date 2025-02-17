5a97a7388b749053540e518dcb53ba5ee4032f21
Elson Otake <elsonotake@yahoo.com.br>
2025-02-16 22:03:22 -0300

    Rename users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  self.table_name = 'exo_cars_users'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings, dependent: :destroy
+  has_many :bookings, dependent: :destroy, foreign_key: 'exo_cars_user_id'
   validates :name, presence: true
 
   def is?(requested_role)
--- db/migrate/20221020213820_add_devise_to_users.rb
+++ db/migrate/20221020213820_add_devise_to_users.rb
@@ -2,7 +2,7 @@
 
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
-    change_table :users do |t|
+    change_table :exo_cars_users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -37,11 +37,11 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.timestamps null: false
     end
 
-    add_index :users, :name,                unique: true
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :exo_cars_users, :name,                unique: true
+    add_index :exo_cars_users, :email,                unique: true
+    add_index :exo_cars_users, :reset_password_token, unique: true
+    # add_index :exo_cars_users, :confirmation_token,   unique: true
+    # add_index :exo_cars_users, :unlock_token,         unique: true
   end
 
   def self.down
