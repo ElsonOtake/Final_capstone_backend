5f1c3613f6c849b990b043c271de217e893dbecc
Elson Otake <elsonotake@yahoo.com.br>
2026-09-11 20:29:26 -0300

    add booking models tests

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Booking, type: :model do
     @car = Vehicle.create(model: 'Impreza', description: 'Good Car', year: '1996', brand: 'Subaru', color: 'Red',
                           country: 'Japan', power: '310 HP', max_speed: '180 mph', acceleration: '0-100/5.6s',
                           price: 100)
-    @booking = Booking.create(user_id: @user.id, vehicle_id: @car.id, start_date: '22/10/2022', end_date: '25/11/2022',
+    @booking = Booking.create(user_id: @user.id, vehicle_id: @car.id, start_date: '2022-10-22', end_date: '2022-11-25',
                               city: 'Manizales')
   end
 
@@ -38,6 +38,29 @@ RSpec.describe Booking, type: :model do
     it 'Should have a user id related' do
       @booking.user_id = nil
       expect(@booking).to_not be_valid
+    end
+
+    it 'End date should be after the start date' do
+      @booking.start_date = '2022-11-20'
+      @booking.end_date = '2022-11-10'
+      expect(@booking).to_not be_valid
+    end
+
+    it 'Should not allow a booking that overlaps an existing booking for the same vehicle' do
+      overlapping_booking = Booking.new(user_id: @user.id, vehicle_id: @car.id, start_date: '2022-11-01',
+                                        end_date: '2022-11-10', city: 'Manizales')
+      expect(overlapping_booking).to_not be_valid
+    end
+
+    it 'Should allow a non-overlapping booking for the same vehicle' do
+      non_overlapping_booking = Booking.new(user_id: @user.id, vehicle_id: @car.id, start_date: '2022-12-01',
+                                            end_date: '2022-12-10', city: 'Manizales')
+      expect(non_overlapping_booking).to be_valid
+    end
+
+    it 'Should allow updating an existing booking without conflicting with itself' do
+      @booking.city = 'Cali'
+      expect(@booking).to be_valid
     end
   end
 end
