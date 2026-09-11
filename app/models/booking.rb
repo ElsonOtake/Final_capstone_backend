0438a857d56035fd62ac9c07c7f35ec26288f5c0
Elson Otake <elsonotake@yahoo.com.br>
2026-09-11 17:12:52 -0300

    add date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :vehicle
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :end_date, comparison: { greater_than: :start_date }
   validates :city, presence: true
 end
