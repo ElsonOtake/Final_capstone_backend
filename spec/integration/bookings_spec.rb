645c28ce969b11baf9243d40714edd3c30b663c6
Elson Otake <elsonotake@yahoo.com.br>
2026-09-11 22:33:44 -0300

    update bookings integration tests

--- spec/integration/bookings_spec.rb
+++ spec/integration/bookings_spec.rb
@@ -1,6 +1,16 @@
 require 'swagger_helper'
 
 describe 'Bookings' do
+  before(:each) do
+    @user = User.create(name: 'booking_user', email: 'booking_user@example.com', password: 'password123')
+    post '/api/v1/auth/login', params: { name: 'booking_user', password: 'password123' }.to_json
+    @token = JSON.parse(response.body).with_indifferent_access[:token]
+ 
+    @vehicle = Vehicle.create(model: 'foo', price: 100)
+    @booking = @vehicle.bookings.create(start_date: '2022-10-02', end_date: '2022-11-14', city: 'Rome',
+                                        user_id: @user.id)
+  end
+
   path '/api/v1/vehicles/{vehicle_id}/bookings' do
     get 'Retrieves bookings' do
       security [{ ApiKeyAuth: [] }]
@@ -10,7 +20,8 @@ describe 'Bookings' do
       parameter name: :vehicle_id, in: :path, type: :integer, required: true, description: 'Vehicle identification'
 
       response '200', 'OK' do
-        schema type: :object, properties: {
+        schema type: :array, items: {
+          type: :object, properties: {
                                 id: { type: :integer },
                                 start_date: { type: :string },
                                 end_date: { type: :string },
@@ -21,18 +32,22 @@ describe 'Bookings' do
                                 updated_at: { type: :string }
                               },
                required: %w[start_date end_date city vehicle_id user_id]
+        }
 
-        let(:vehicle_id) { Vehicle.create(model: 'foo', price: 100).id }
+        let(:vehicle_id) { @vehicle.id }
+        let(:Authorization) { @token }
         run_test!
       end
 
       response '401', 'Unauthorized' do
-        let(:vehicle_id) { 'Unauthorized' }
+        let(:vehicle_id) { @vehicle.id }
+        let(:Authorization) { nil }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:vehicle_id) { 'Vehicle not found' }
+        let(:vehicle_id) { 999_999 }
+        let(:Authorization) { @token }
         run_test!
       end
     end
@@ -59,17 +74,23 @@ describe 'Bookings' do
                               },
                required: %w[start_date end_date city vehicle_id user_id]
 
-        let(:id) { Booking.create(start_date: '2022-10-02', end_date: '2022-11-14', city: 'Rome', user_id: 1).id }
+        let(:vehicle_id) { @vehicle.id }
+        let(:id) { @booking.id }
+        let(:Authorization) { @token }
         run_test!
       end
 
       response '401', 'Unauthorized' do
-        let(:id) { 'Unauthorized' }
+        let(:vehicle_id) { @vehicle.id }
+        let(:id) { @booking.id }
+        let(:Authorization) { nil }
         run_test!
       end
 
       response '404', 'Not found' do
-        let(:id) { 'Booking not found' }
+        let(:vehicle_id) { @vehicle.id }
+        let(:id) { 999_999 }
+        let(:Authorization) { @token }
         run_test!
       end
     end
@@ -94,12 +115,16 @@ describe 'Bookings' do
       }
 
       response '200', 'OK' do
-        let(:booking) { { start_date: '2022-10-02', end_date: '2022-11-14', city: 'Rome', user_id: 1 } }
+        let(:vehicle_id) { @vehicle.id }
+        let(:booking) { { start_date: '2022-12-01', end_date: '2022-12-05', city: 'Rome', user_id: @user.id } }
+        let(:Authorization) { @token }
         run_test!
       end
-
+ 
       response '422', 'Unprocessable entity' do
+        let(:vehicle_id) { @vehicle.id }
         let(:booking) { { city: 'Rome' } }
+        let(:Authorization) { @token }
         run_test!
       end
     end
@@ -114,7 +139,8 @@ describe 'Bookings' do
       parameter name: :user_id, in: :path, type: :integer, required: true, description: 'User identification'
 
       response '200', 'OK' do
-        schema type: :object, properties: {
+        schema type: :array, items: {
+          type: :object, properties: {
                                 id: { type: :integer },
                                 start_date: { type: :string },
                                 end_date: { type: :string },
@@ -125,18 +151,22 @@ describe 'Bookings' do
                                 updated_at: { type: :string }
                               },
                required: %w[start_date end_date city vehicle_id user_id]
+        }
 
-        let(:user_id) { User.create(name: 'Ariel', email: 'ariel@capstone.com', password: 'password').id }
+        let(:user_id) { @user.id }
+        let(:Authorization) { @token }
         run_test!
       end
-
+ 
       response '401', 'Unauthorized' do
-        let(:user_id) { 'Unauthorized' }
+        let(:user_id) { @user.id }
+        let(:Authorization) { nil }
         run_test!
       end
-
+ 
       response '404', 'Not found' do
-        let(:user_id) { 'User not found' }
+        let(:user_id) { 999_999 }
+        let(:Authorization) { @token }
         run_test!
       end
     end
@@ -163,17 +193,23 @@ describe 'Bookings' do
                               },
                required: %w[start_date end_date city vehicle_id user_id]
 
-        let(:id) { Booking.create(start_date: '2022-10-02', end_date: '2022-11-14', city: 'Rome', user_id: 1).id }
+        let(:user_id) { @user.id }
+        let(:id) { @booking.id }
+        let(:Authorization) { @token }
         run_test!
       end
-
+ 
       response '401', 'Unauthorized' do
-        let(:id) { 'Unauthorized' }
+        let(:user_id) { @user.id }
+        let(:id) { @booking.id }
+        let(:Authorization) { nil }
         run_test!
       end
-
+ 
       response '404', 'Not found' do
-        let(:id) { 'Booking not found' }
+        let(:user_id) { @user.id }
+        let(:id) { 999_999 }
+        let(:Authorization) { @token }
         run_test!
       end
     end
@@ -198,12 +234,16 @@ describe 'Bookings' do
       }
 
       response '200', 'OK' do
-        let(:booking) { { start_date: '2022-10-02', end_date: '2022-11-14', city: 'Rome', vehicle_id: 1 } }
+        let(:user_id) { @user.id }
+        let(:booking) { { start_date: '2022-12-10', end_date: '2022-12-15', city: 'Rome', vehicle_id: @vehicle.id } }
+        let(:Authorization) { @token }
         run_test!
       end
-
+ 
       response '422', 'Unprocessable entity' do
+        let(:user_id) { @user.id }
         let(:booking) { { city: 'Rome' } }
+        let(:Authorization) { @token }
         run_test!
       end
     end
