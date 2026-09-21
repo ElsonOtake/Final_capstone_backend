8f281be00961173547e7f4bd8c0d75940640a744
Elson Otake <elsonotake@yahoo.com.br>
2026-09-21 17:28:50 -0300

    migrate tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,91 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2022_10_20_213820) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_21_180132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "bookings", force: :cascade do |t|
-    t.date "start_date"
-    t.date "end_date"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.date "end_date"
+    t.date "start_date"
+    t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "vehicle_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["vehicle_id"], name: "index_bookings_on_vehicle_id"
   end
 
-  create_table "exo_cars_users", force: :cascade do |t|
-    t.string "name"
-    t.string "role"
+  create_table "exo_active_storage_attachments", force: :cascade do |t|
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
+    t.index ["blob_id"], name: "index_exo_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_exo_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "exo_active_storage_blobs", force: :cascade do |t|
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.string "content_type"
+    t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.index ["key"], name: "index_exo_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "exo_active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_exo_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "exo_cars_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "name"
     t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.string "role"
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_exo_cars_users_on_email", unique: true
     t.index ["name"], name: "index_exo_cars_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_exo_cars_users_on_reset_password_token", unique: true
   end
 
   create_table "galleries", force: :cascade do |t|
-    t.string "photo"
-    t.bigint "vehicle_id", null: false
     t.datetime "created_at", null: false
+    t.string "photo"
     t.datetime "updated_at", null: false
+    t.bigint "vehicle_id", null: false
     t.index ["vehicle_id"], name: "index_galleries_on_vehicle_id"
   end
 
   create_table "vehicles", force: :cascade do |t|
-    t.string "model"
-    t.string "description"
-    t.string "year"
+    t.string "acceleration"
     t.string "brand"
     t.string "color"
     t.string "country"
-    t.string "power"
-    t.string "max_speed"
-    t.string "acceleration"
-    t.integer "price"
     t.datetime "created_at", null: false
+    t.string "description"
+    t.string "max_speed"
+    t.string "model"
+    t.string "power"
+    t.integer "price"
     t.datetime "updated_at", null: false
+    t.string "year"
   end
 
   add_foreign_key "bookings", "exo_cars_users", column: "user_id"
   add_foreign_key "bookings", "vehicles"
+  add_foreign_key "exo_active_storage_attachments", "exo_active_storage_blobs", column: "blob_id"
+  add_foreign_key "exo_active_storage_variant_records", "exo_active_storage_blobs", column: "blob_id"
   add_foreign_key "galleries", "vehicles"
 end
